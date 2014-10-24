593d0330c3cd4fe62ad086a587f9c42b6fe3f4ac
Saliutama <jsaliutama@gmail.com>
2014-10-23 21:28:15 -0400

    Create Sudoku.java
    Created the skeleton for the project.

---
+++ Sudoku.java
@@ -0,0 +1,33 @@
+public class Sudoku {
+    public Sudoku() {
+        
+    }
+    
+    public Sudoku(int[][] board) {
+        
+    }
+    
+    public int[][] board() {
+        
+    }
+    
+    public boolean candidates(int row, int column) {
+        
+    }
+    
+    public boolean isSolved() {
+        
+    }
+    
+    public void solve() {
+        
+    }
+    
+    public boolean nakedSingles() {
+        
+    }
+    
+    public boolean hiddenSingles() {
+        
+    }
+}
